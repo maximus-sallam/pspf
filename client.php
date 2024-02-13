95d2ddcd0d3318fd75f62aa3c0475f1bea40c2b3
Maximus Sallam <maximus.sallam@gmail.com>
2024-02-13 03:55:11 -0600

    Blocked access to API directory and began work on client.php file.

---
+++ client.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name: Zebra API
+Plugin URI: https://maximus-sallam.com
+Description: Plugin for extracting information from remote Festisoft API
+Author: Maximus Sallam
+Version: 1.0
+Author URI: http://maximus-sallam.com
+*/
+
